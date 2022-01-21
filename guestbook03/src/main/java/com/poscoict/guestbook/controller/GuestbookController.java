aab12dad7d3660ea8e574c0d78e7eb8105bb4645
BIT <BIT@DESKTOP-BM40P2C>
2022-01-21 17:48:18 +0900

    guestbook

--- guestbook03/src/main/java/com/poscoict/guestbook/controller/GuestbookController.java
+++ guestbook03/src/main/java/com/poscoict/guestbook/controller/GuestbookController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -22,20 +23,19 @@ public class GuestbookController {
 		model.addAttribute("list", list);
 		return "/WEB-INF/views/index.jsp";
 	}
-	@RequestMapping("/add")
+	@RequestMapping(value="/add", method=RequestMethod.POST)
 	public String add(GuestbookVo vo) {
 		guestbookRepository.insert(vo);
 		return "redirect:/";
 	}
-	@RequestMapping(value="/delete", method=RequestMethod.GET)
-	public String delete() {
-		
+	@RequestMapping(value="/delete/{no}", method=RequestMethod.GET)
+	public String delete(@PathVariable("no") Long no, Model model) {
+		model.addAttribute("no", no);
 		return "/WEB-INF/views/delete.jsp";
 	}
 	@RequestMapping(value="/delete", method=RequestMethod.POST)
 	public String delete(GuestbookVo vo) {
 		guestbookRepository.delete(vo);
-		System.out.println(vo);
 		return "redirect:/";
 	}
 }
